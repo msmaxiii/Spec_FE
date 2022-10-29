2d8f99bfc94070aafa0394bce11154da8ab0cddf
msmaxiii <maxwellrg@gmail.com>
2022-10-29 00:11:31 -0500

    added css

--- app.js
+++ app.js
@@ -1,27 +1,7 @@
-let books = document.getElementById('books');
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
 
-let booksItemsData=[{},{},{},{},{}]
-id:"1"
-Title:"Cat Kid"
-Genre:"Kids"
-Price:5.9
-images:"i"
-
-let booksStore =()=>{
-    return (shop.innerHTML= booksItemsData.map)
-    <div class="genre">
-    <img src="images/cat.webp"alt="kid book" width="220px"
-    <div class="details">
-        <h2>Title:</h2>
-        <h4>Genre:</h4>
-     <div class="price-quantity">
-         <h3>price:</h3> 
-         <i class="bi bi-heart"></i> 
-     <div class="buttons">
-         <i class="bi bi-plus"></i>
-         <div class="quanitity">0</div>
-         <i class="bi bi-dash"></i>
-             </div>     
-         </div>
-     </div>
+menu.onclick =(){
+    menu.classList.toggle('fa-times');
+    menu.classList.toggle('active');
 }
